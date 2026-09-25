f049950d655ff810ce252cb6854695f5a7c85db3
GinoTheBull <thebullgino@gmail.com>
2026-09-25 10:04:08 -0400

    Improve emotion detection with error handling
    Added error handling for API response status codes and refactored emotion score extraction.

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -7,20 +7,30 @@ def emotion_detector(text_to_analyze):  # Define a function named sentiment_anal
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"} # Set the headers required for the API request
     response = requests.post(url, json = myobj, headers=header) # Send a POST request to the API with the text and headers
 
-   # Parsing the JSON response from the API
-    formatted_response = response.json() # Change to response.json() to handle non-str
+    if response.status_code == 200:
+       # If the response status code is 200, parse the JSON response
+        formatted_response = response.json() # Change to response.json() to handle non-str
+        emotion_predictions_root = formatted_response.get("emotionPredictions")
+        emotion_predictions_first = emotion_predictions_root[0]
+        emotion_scores = emotion_predictions_first.get("emotion")
 
-    emotion_predictions_root = formatted_response.get("emotionPredictions")
-    emotion_predictions_first = emotion_predictions_root[0]
-    emotion_scores = emotion_predictions_first.get("emotion")
+        anger_score = emotion_scores.get("anger")
+        disgust_score = emotion_scores.get("disgust")
+        fear_score = emotion_scores.get("fear")
+        joy_score = emotion_scores.get("joy")
+        sadness_score = emotion_scores.get("sadness")
 
-    anger_score = emotion_scores.get("anger")
-    disgust_score = emotion_scores.get("disgust")
-    fear_score = emotion_scores.get("fear")
-    joy_score = emotion_scores.get("joy")
-    sadness_score = emotion_scores.get("sadness")
+        dominant_emotion_score = max(emotion_scores, key=emotion_scores.get)
 
-    dominant_emotion_score = max(emotion_scores, key=emotion_scores.get)
+    # If the response status code is 400, set to None
+    elif response.status_code == 400:
+        anger_score = None
+        disgust_score = None
+        fear_score = None
+        joy_score = None
+        sadness_score = None
+
+        dominant_emotion_score = None
 
     return {
         'anger': anger_score,
